4d6ed10cc7133f102412ce096da64c3a3c3e22bc
Cedomir Rackov <car492@gmail.com>
2017-08-12 14:01:21 +0200

    Fix scroll

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -10,20 +10,18 @@ export class MenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    window.addEventListener('load', e => {
-      let firstNav = document.getElementsByTagName('nav')[0]
+    let firstNav = document.getElementsByTagName('nav')[0]
 
-      let prevScroll = 0
-      window.addEventListener('scroll', e => {
-          if (window.scrollY < firstNav.parentElement.offsetTop && firstNav.classList.contains('fixed')) {
-              firstNav.classList.remove('fixed')
-              console.log('removed class')
-          }
-          else if (window.scrollY > firstNav.parentElement.offsetTop && !firstNav.classList.contains('fixed')) {
-              firstNav.classList.add('fixed')
-              console.log('added class')
-          }
-      })
+    let prevScroll = 0
+    window.addEventListener('scroll', e => {
+        if (window.scrollY < firstNav.parentElement.offsetTop && firstNav.classList.contains('fixed')) {
+            firstNav.classList.remove('fixed')
+            console.log('removed class')
+        }
+        else if (window.scrollY > firstNav.parentElement.offsetTop && !firstNav.classList.contains('fixed')) {
+            firstNav.classList.add('fixed')
+            console.log('added class')
+        }
     })
   }
 
